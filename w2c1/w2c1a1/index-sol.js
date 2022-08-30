b22f7f1cda7698440611693395f86ec7c09f0a62
sweetflute <sweetflute@gmail.com>
2022-08-30 19:54:00 -0400

    update w2c1a1 solution

--- w2c1/w2c1a1/index-sol.js
+++ w2c1/w2c1a1/index-sol.js
@@ -19,97 +19,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 	document.getElementById("more-review").addEventListener("click", function(){
 		let movieCP = newMovie[0]
-		// let movieCPNode = document.getElementsByClassName("panel-body")[3]
-		// let movieCPNodeText = movieCPNode.getElementsByClassName("text")
-		// movieCPNodeText[0].querySelector(".caption-display > span").textContent = movieCP[0]
-		// movieCPNodeText[0].getElementsByClassName("status")[0].textContent = movieCP[1]
-		// movieCPNodeText[0].getElementsByClassName("status")[1].textContent = movieCP[2]
-		// movieCPNodeText[0].getElementsByClassName("status")[2].textContent = movieCP[3]
-		// let movieCPNodePoster = movieCPNode.getElementsByClassName("poster")
-		// movieCPNodePoster[0].querySelector("img").setAttribute("src", movieCP[4])
+		let movieCPNode = document.getElementsByClassName("panel-body")[3]
+		let movieCPNodeText = movieCPNode.getElementsByClassName("text")
+		movieCPNodeText[0].querySelector(".caption-display > span").textContent = movieCP[0]
+		movieCPNodeText[0].getElementsByClassName("status")[0].textContent = movieCP[1]
+		movieCPNodeText[0].getElementsByClassName("status")[1].textContent = movieCP[2]
+		movieCPNodeText[0].getElementsByClassName("status")[2].textContent = movieCP[3]
+		let movieCPNodePoster = movieCPNode.getElementsByClassName("poster")
+		movieCPNodePoster[0].querySelector("img").setAttribute("src", movieCP[4])
 
 
-		// let movieALNM = newMovie[1]
-		// let movieALNMNode = document.getElementsByClassName("panel-body")[4]
-		// let movieALNMNodeText = movieALNMNode.getElementsByClassName("text")
-		// movieALNMNodeText[0].querySelector(".caption-display > span").textContent = movieALNM[0]
-		// movieALNMNodeText[0].getElementsByClassName("status")[0].textContent = movieALNM[1]
-		// movieALNMNodeText[0].getElementsByClassName("status")[1].textContent = movieALNM[2]
-		// movieALNMNodeText[0].getElementsByClassName("status")[2].textContent = movieALNM[3]
-		// let movieALNMNodePoster = movieALNMNode.getElementsByClassName("poster")
-		// movieALNMNodePoster[0].querySelector("img").setAttribute("src", movieALNM[4])
-
-		/* Adding More Review */
-		let mainNode = document.getElementById("main")
-		let panelBodyNode = document.createElement("div")
-		panelBodyNode.setAttribute("class", "panel-body")
-
-		mainNode.appendChild(panelBodyNode)
-
-		let textNode = document.createElement("div")
-		textNode.setAttribute("class", "text")
-
-		panelBodyNode.appendChild(textNode)
-
-		let captionNode = document.createElement("div")
-		captionNode.setAttribute("class", "caption-display")
-
-		textNode.appendChild(captionNode)
-
-		let titleNode = document.createElement("span")
-		let titleTextNode = document.createTextNode(movieCP[0]);
-		titleNode.appendChild(titleTextNode);
-		captionNode.appendChild(titleNode)
-
-		let authorNode = document.createElement("span")
-		authorNode.setAttribute("class", "status")
-		let authorTextNode = document.createTextNode(movieCP[1]);
-		authorNode.appendChild(authorTextNode);
-		textNode.appendChild(authorNode)
-
-		let dateNode = document.createElement("span")
-		dateNode.setAttribute("class", "status")
-		let dateTextNode = document.createTextNode(movieCP[2]);
-		dateNode.appendChild(dateTextNode);
-		textNode.appendChild(dateNode)
-
-		let urlNode = document.createElement("span")
-		urlNode.setAttribute("class", "status")
-		let urlTextNode = document.createTextNode(movieCP[3]);
-		urlNode.appendChild(urlTextNode);
-		textNode.appendChild(urlNode)
-
-		let aNode = document.createElement("a")
-		aNode.setAttribute("href", "#")
-		aNode.setAttribute("class", "hide")
-		let aTextNode = document.createTextNode("hide this review");
-		aNode.appendChild(aTextNode)
-		textNode.appendChild(aNode)
-
-		let posterNode = document.createElement("div")
-		posterNode.setAttribute("class","poster")
-		panelBodyNode.appendChild(posterNode)
-
-		let imgNode = document.createElement("img")
-		imgNode.setAttribute("class", "image")
-		imgNode.setAttribute("src", movieCP[4])
-		posterNode.appendChild(imgNode)
-
-
-		
+		let movieALNM = newMovie[1]
+		let movieALNMNode = document.getElementsByClassName("panel-body")[4]
+		let movieALNMNodeText = movieALNMNode.getElementsByClassName("text")
+		movieALNMNodeText[0].querySelector(".caption-display > span").textContent = movieALNM[0]
+		movieALNMNodeText[0].getElementsByClassName("status")[0].textContent = movieALNM[1]
+		movieALNMNodeText[0].getElementsByClassName("status")[1].textContent = movieALNM[2]
+		movieALNMNodeText[0].getElementsByClassName("status")[2].textContent = movieALNM[3]
+		let movieALNMNodePoster = movieALNMNode.getElementsByClassName("poster")
+		movieALNMNodePoster[0].querySelector("img").setAttribute("src", movieALNM[4])
 
 	})
-
-	/* Deleteing A Review */
-
-	let delNodes = document.getElementsByClassName("hide")
-
-	Array.from(delNodes).forEach(element => {
-		element.addEventListener("click", function(){
-			this.parentElement.parentElement.remove()
-		})
-	})
-
 
 
 }, false);
